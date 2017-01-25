2f0acf64b61c769559446049712140bfbca98a37
Bryan Jackson <bjack13@gmail.com>
2017-01-25 12:49:40 -0700

    half way done with auth still getting error

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const cors = require('cors');
 const massive = require('massive');
-
+// const cookieParser = require('cookie-parser');
 
 const config = require('./config.json');
 
 const app = express();
+const port = config.port;
 
+app.use(bodyParser.json());
+app.use(cors());
 
+// app.use(cookieParser(config.secret));
 app.use(session({
-  secret: config.secret,
-  resave: true,
-  saveUninitialized: false
+  // secret: config.secret,
+  secret: 'bacon',
+  saveUninitialized: true,
+  resave: true
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(bodyParser.json());
-app.use(cors());
 app.use(express.static(__dirname + "/dist"));
 
 //setup database connection
@@ -39,7 +43,7 @@ passport.use('facebook', new FacebookStrategy ({
   clientID: config.facebook.clientId,
   clientSecret: config.facebook.clientSecret,
   callbackURL:'http://localhost:3000/auth/facebook/callback',
-  profileFields:[]
+  profileFields:['id', 'displayName']
 },(accessToken,refreashToken, profile, done) =>{
   // Access the database
 
@@ -48,6 +52,7 @@ passport.use('facebook', new FacebookStrategy ({
     if (!user) {
       console.log('CREATING USER');
       db.createUserFacebook([profile.displayName, profile.id], function(err, user) {
+        console.log('USER CREATED', user);
         return done(err, user, {scope: 'all'});
       });
     } else {
@@ -60,16 +65,33 @@ passport.serializeUser((user, done) => {
   return done(null, user);
 });
 passport.deserializeUser((user, done) => {
-  return done(null, user);
+  db.getUserById([id], function(err, user) {
+    user = user[0];
+    if (err) console.log(err);
+    else console.log('RETRIEVED USER');
+    console.log(user);
+    done(null, user);
+  });
 });
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {successRedirect: '/' }), function(req, res) {
+    res.status(200).send(req.user);
+});
+
+app.get('/auth/me', function(req, res) {
+  if (!req.user) return res.sendStatus(404);
   res.status(200).send(req.user);
+});
+
+app.get('/auth/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 
 app.listen(config.port, function () {
-  console.log("it is ALIVE!!  @"+config.port);
+  console.log("it is ALIVE!!  @"+ port);
 });
