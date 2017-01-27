1e298e979d8a5079fdadc7c4e03bd550d075b4ac
Bryan Jackson <bjack13@gmail.com>
2017-01-27 09:49:52 -0700

    refactor code over to Ctrl

--- index.js
+++ index.js
@@ -65,69 +65,24 @@ passport.deserializeUser(oAuthCtrl.deserializeUser);
 // FacebookStrategy endpoints
 app.get('/auth/facebook', passport.authenticate('facebook', {scope:['email']}));
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {failureRedirect: '/login' }), oAuthCtrl.facebookCallback);
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/login' }), oAuthCtrl.facebookCallback);
 
 // GoogleStrategy endpoints
-
 app.get('/auth/google', passport.authenticate('google', {scope:['https://www.googleapis.com/auth/plus.login',
        'https://www.googleapis.com/auth/plus.profile.emails.read']}, (req, res) => {
     const token = jwt.sign();
 }));
-
 app.get('/auth/google/callback',
   passport.authenticate('google', {successRedirect: '/' , failureRedirect: '/login'}), function(req, res) {
     res.status(200).send(req.user);
 });
 
 // LocalStrategy endpoints
+app.post('/login', oAuthCtrl.localLogin);
+app.post('/api/user', oAuthCtrl.createUserLocal);
 
-// app.get('/',
-//   function(req, res) {
-//     res.render('main', { user: req.user });
-//   });
-//
-// app.get('/login',
-//   function(req, res){
-//     res.render('login');
-//   });
-
-app.post('/login', function(req, res, done){
-      db.getUserByUsername([req.body.username], function(err, user) {
-        if(!user.length){
-          res.json(false);
-          return;
-        }
-        if (err) { console.log(err); return;}
-        if (user[0].password != req.body.password) {
-        res.json(false);
-        }
-        else {
-          res.json(true);
-          req.session.passport.user = user[0];
-        }
-      });
-    });
-
-
-
-app.post('/api/user', function (req, res) {
-  console.log(req.body);
-
-  db.createUserLocal([req.body.username,req.body.password,req.body.email], function (err, user) {
-    res.status(200).json('success');
-  });
-});
-
-// app.get('/login', function(req, res) {
-//   res.render('login');
-// });
-
-app.get('/auth/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
+// Logout
+app.get('/auth/logout', oAuthCtrl.logout);
 
 app.listen(config.port, function () {
   console.log("it is ALIVE!!  @"+ port);
--- serverCtrls/oAuthCtrl.js
+++ serverCtrls/oAuthCtrl.js
@@ -23,9 +23,9 @@ module.exports= {
     });
   },
   facebookAuth: (accessToken,refreashToken, profile, cb) => {
-    db.getUserByFacebookId([profile.id], function(err, user) {
+    db.getUserByFacebookId([profile.id], (err, user) => {
       if (!user.length) {
-        db.getUserByEmail([profile.emails[0].value], function (err, user) {
+        db.getUserByEmail([profile.emails[0].value], (err, user) => {
           if (user.length) {
             db.updateUserFacebookId([profile.id, profile.emails[0].value],function (err, user) {
               return cb ( err, user[0], {scope: 'all'});
@@ -46,12 +46,12 @@ module.exports= {
   },
   deserializeUser: (user, done) => {
     if (!user.facebookid) {
-      db.getUserByUsername([user.username], function(err, u){
+      db.getUserByUsername([user.username], (err, u) => {
         return done(null,u);
       });
     }
     else {
-      db.getUserByFacebookId([user.facebookid], function(err, user) {
+      db.getUserByFacebookId([user.facebookid], (err, user) => {
         done(null, user);
       });
     }
@@ -59,7 +59,37 @@ module.exports= {
   },
   facebookCallback: (req, res) => {
     res.redirect('/#/');
+    console.log('*** 1 ***');
     console.log(req.session);
   },
+  logout : function(req, res) {
+    req.logout();
+    res.redirect('/');
+  },
+  createUserLocal: (req, res) => {
+    console.log('*** 2 ***');
+    console.log(req.body);
+
+    db.createUserLocal([req.body.username,req.body.password,req.body.email], function (err, user) {
+      res.status(200).json('success');
+    });
+  },
+  localLogin : (req, res, done) => {
+    db.getUserByUsername([req.body.username], function(err, user) {
+      if(!user.length){
+        res.json(false);
+        return;
+      }
+      if (err) { console.log(err); return;}
+      if (user[0].password != req.body.password) {
+      res.json(false);
+      }
+      else {
+        res.json(true);
+        req.session.passport.user = user[0];
+      }
+    });
+  },
+
 
 };
