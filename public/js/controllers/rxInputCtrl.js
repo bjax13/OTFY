777f5843a543c48e8aa6ec74511fd3e23c52bfc9
Bryan Jackson <bjack13@gmail.com>
2017-01-31 19:32:48 -0700

    meta for mobile

--- public/js/controllers/rxInputCtrl.js
+++ public/js/controllers/rxInputCtrl.js
@@ -2,12 +2,19 @@ angular.module('app')
   .controller('rxInputCtrl', function ($scope, rxSrvc ) {
     $scope.test = rxSrvc.test;
 
+    $scope.detailsToggle = true;
+
+    // $scope.toggleDetails = () =>{
+    //    $scope.detailsToggle = !$scope.detailsToggle;
+    //    console.log($scope.detailsToggle);
+    //
+    // };
 
     $scope.getProducts = function () {
 
       rxSrvc.populateProductList().then(function (response) {
         $scope.products = response.data;
-
+        console.log(response.data);
       });
     };
 
